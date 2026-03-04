1e4c6aa80b04775bc89b0fe5ff0082d7004bd9ab
koko-yl <taborinstein@gmail.com>
2026-03-03 19:38:51 -0500

    icons

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use hail_pdk::{ConfigInput, HailPlugin, PluginFFI, core::types::Comparison, sdl2::{pixels::Color, rect::Rect, render::{Texture, TextureQuery}}, utils::{ConfigFont, PdkUtilErr, TextRenderer}};
+use hail_pdk::{ConfigInput, HailPlugin, PluginFFI, core::types::Comparison, sdl2::{image::LoadTexture, pixels::Color, rect::Rect, render::{ScaleMode, Texture, TextureQuery}}, utils::{ConfigFont, PdkUtilErr, TextRenderer}};
 use serde::Deserialize;
 
 #[derive(PluginFFI)]
 struct SegmentPlugin {
     h: u32,
-    textures: [Result<Texture, PdkUtilErr>; 4]
+    textures: [Result<Texture, PdkUtilErr>; 5],
+    images: [Option<Texture>; 5]
 }
 
 
@@ -31,18 +32,26 @@ impl HailPlugin for SegmentPlugin {
         let creator = c.texture_creator();
         let mut renderer = TextRenderer::new(ssrc, font).unwrap();
         let textures = [
-            renderer.render_text_line("Comparison: Personal Best", color, &creator),
-            renderer.render_text_line("Comparison: Golds", color, &creator),
-            renderer.render_text_line("Comparison: Average", color, &creator),
-            renderer.render_text_line("Comparison: None", color, &creator)
+            renderer.render_text_line(" Comparison: ", color, &creator),
+            renderer.render_text_line(" Personal Best", color, &creator),
+            renderer.render_text_line(" Golds", color, &creator),
+            renderer.render_text_line(" Average", color, &creator),
+            renderer.render_text_line("None", color, &creator)
         ];
-        Ok(Self { textures, h: renderer.line_height })
+        let images = [
+            None,
+            creator.load_texture_bytes(include_bytes!("../assets/pb.png")).ok(),
+            creator.load_texture_bytes(include_bytes!("../assets/gold.png")).ok(),
+            creator.load_texture_bytes(include_bytes!("../assets/avg.png")).ok(),
+            None
+        ];
+        
+        Ok(Self { textures, h: renderer.line_height, images })
     }
     
     fn update(&mut self, _: &hail_pdk::core::HailState, _: &hail_pdk::InputState) {}
     
     fn min_dims(&self) -> Option<(u32, u32)> {
-        // println!("segment plugin min_dims");
         Some((0, 0))
     }
     
@@ -51,28 +60,55 @@ impl HailPlugin for SegmentPlugin {
     }
 
     fn draw(&mut self, s: &hail_pdk::core::HailState, c: &mut hail_pdk::sdl2::render::WindowCanvas) {
-        // println!("segment plugin draw");
         c.set_draw_color((0, 0, 0, 0));
         c.clear();
-        let tex = match s.comparison {
-            Comparison::PersonalBest => &self.textures[0],
-            Comparison::Golds => &self.textures[1],
-            Comparison::Average => &self.textures[2],
-            Comparison::None => &self.textures[3],
+        let n = match s.comparison {
+            Comparison::PersonalBest => 1,
+            Comparison::Golds => 2,
+            Comparison::Average => 3,
+            Comparison::None => 4,
         };
-        // let tex = tex.unwrap();
+        let tex = &self.textures[n];
         let vp = c.viewport();
-        let TextureQuery { width, height, .. } = tex.as_ref().unwrap().query();
-        let xpos = 8;//(vp.width() as i32 - width as i32) / 2;
+        let mut xpos = 8;
+        
+        let head_tex = self.textures[0].as_ref().unwrap();
+        let TextureQuery { width, height, .. } = head_tex.query();
         let ypos = (vp.height() as i32 - height as i32) / 2;
+        
+        
         c.copy(
-            &tex.as_ref().unwrap(),
+            &head_tex,
             None,
             Rect::new(xpos, ypos, width, height),
         )
         .unwrap();
-        
-        // hail_pdk::utils::TextRenderer
+        xpos += width as i32;
+        if let Some(img) = self.images[n].as_mut() {
+            img.set_scale_mode(ScaleMode::Best);
+            let d = (height as f32 * 0.8) as u32;
+            let pad = (height - d) / 2;
+            c.copy(
+                img,
+                None,
+                Rect::new(
+                    xpos,
+                    ypos, 
+                    d, 
+                    d
+                )
+            ).unwrap();
+    
+            xpos += (height - pad - pad) as i32;
+        }
+
+        let TextureQuery { width, height, .. } = tex.as_ref().unwrap().query();
+        c.copy(
+            tex.as_ref().unwrap(),
+            None,
+            Rect::new(xpos, ypos, width, height),
+        )
+        .unwrap();
     }
     
     type Config = SConfig;
